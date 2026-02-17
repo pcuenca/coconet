d17f17dfb186c07dba1b877fd0ecccd1f5e42f03
Pedro Cuenca <pedro@huggingface.co>
2026-02-17 18:33:45 +0100

    Unhardcode image

--- test_filternet.py
+++ test_filternet.py
@@ -1,3 +1,6 @@
+import argparse
+from pathlib import Path
+
 from filternet import FilternetPredictor
 from PIL import Image
 
@@ -6,13 +9,28 @@ checkpoint = "/home/pedro/code/photo-editing/filternet/models/p-tebcwh-batch-nor
 predictor = FilternetPredictor(checkpoint)
 print(predictor)
 
-from diffusers.utils import load_image
-image = load_image("images/cat-crappy.png")
+parser = argparse.ArgumentParser(description="Run Filternet on a single image.")
+parser.add_argument("image", help="Path to input image.")
+args = parser.parse_args()
 
-prediction = predictor.predict(image)
+input_path = Path(args.image)
+image = Image.open(input_path).convert("RGB")
 
-prediction.image.save("filternet-predicted.png")
+square_for_prediction = image.resize(
+    (predictor.image_size, predictor.image_size), Image.Resampling.BILINEAR
+)
+prediction = predictor.predict(square_for_prediction)
+intensities = [float(prediction.filters[name]) for name in predictor.filter_names]
+output_image = predictor.apply_filters(image, intensities)
+
+if input_path.suffix:
+    output_path = input_path.with_name(f"{input_path.stem}-filternet{input_path.suffix}")
+else:
+    output_path = input_path.with_name(f"{input_path.name}-filternet")
+
+output_image.save(str(output_path))
 print(f"Predicted filters: {prediction.filters}")
+print(f"Saved prediction to: {output_path}")
 
 # TODO: I'm getting params close to the notebook version, but not identical
 # notebook: [-0.01318, -0.07661, -0.2604, 0.15502, -0.75897, -0.39759]
